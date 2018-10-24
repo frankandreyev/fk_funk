5c162297389e2e7db79755587c002f4067082398
frankandreyev <43652964+frankandreyev@users.noreply.github.com>
2018-10-24 19:00:25 +0200

    Update switchMeToCat.py

--- switchMeToCat.py
+++ switchMeToCat.py
@@ -1,13 +1,14 @@
 # switchMetoCat
 # Input :
 # threshold -> max threshold, if the number of values is greater than the threshold, then the numeric value stays numeric
-# num_data -> contains a dataframe with numeric variables only
+# data -> pandas dataframe
 # Output : returns the column names under the threshold in cat and the column names over the threshold in num
 
-def split_switchMetoCat(threshold = 10, data) :
+def split_switchMetoCat (threshold, data) :
     cat = list()
     num = list()
     for col in data.columns :
-        if( (data[col].dtype == 'float') | (data[col].dtype == 'int')) : num.append(col)
-        else : cat.append(col)
+        if( (data[col].dtype == 'float') | (data[col].dtype == 'int')) :
+            if(data[col].nunique() <= threshold) : cat.append(col)
+            else : num.append(col)
     return cat, num
